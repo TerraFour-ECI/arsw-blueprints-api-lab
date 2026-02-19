1c1b596802735b5a151a56724b822116b492a7c5
Elizabeth Correa <elizabeth.correa-s@mail.escuelaing.edu.co>
2026-02-19 10:36:10 -0500

    feat: document API endpoints with springdoc annotations

--- src/main/java/edu/eci/arsw/blueprints/model/Point.java
+++ src/main/java/edu/eci/arsw/blueprints/model/Point.java
@@ -1,3 +1,9 @@
 package edu.eci.arsw.blueprints.model;
 
-public record Point(int x, int y) { }
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "2D coordinate point", example = "{\"x\": 10, \"y\": 20}")
+public record Point(
+    @Schema(description = "X coordinate", example = "10") int x, 
+    @Schema(description = "Y coordinate", example = "20") int y
+) { }
